d0b28c3d4976b7c42bd3a69a292ae83320b57f78
Matthew Vladika <MJVladika@student.fullsail.edu>
2022-09-02 15:35:11 -0400

    Clean Slate

---
+++ app/src/main/java/com/dimplehead/app/Courses.kt
@@ -0,0 +1,5 @@
+package com.dimplehead.app
+
+data class Courses(val courseName : String? = null, val courseAddress : String? = null, val numOfHoles : String? = null,
+                   val coursePar : String? = null, val yearOpened : String? = null, val proOnsite : String? = null,
+                   val drivingRange : String? = null, val rentalClubs : String? = null, val rentalCarts : String? = null)
---
+++ app/src/main/java/com/dimplehead/app/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.dimplehead.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.dimplehead.app.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmitCourse.setOnClickListener {
+
+
+            val courseName = binding.etCourseName
+            val courseAddress = binding.etCourseAddress
+            val numOfHoles = binding.etNumOfHoles
+            val coursePar = binding.etCoursePar
+            val yearOpened = binding.etYearOpened
+            val proOnsite = binding.etProOnsite
+            val drivingRange = binding.etDrivingRange
+            val rentalClubs = binding.etRentalClubs
+            val rentalCarts = binding.etRentalCarts
+
+            val courseNameTXT = courseName.text.toString()
+            val courseAddressTXT = courseAddress.text.toString()
+            val numOfHolesTXT = numOfHoles.text.toString()
+            val courseParTXT = coursePar.text.toString()
+            val yearOpenedTXT = yearOpened.text.toString()
+            val proOnsiteTXT = proOnsite.text.toString()
+            val drivingRangeTXT = drivingRange.text.toString()
+            val rentalClubsTXT = rentalClubs.text.toString()
+            val rentalCartsTXT = rentalCarts.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("Courses")
+            val course = Courses(courseNameTXT, courseAddressTXT, numOfHolesTXT, courseParTXT, yearOpenedTXT, proOnsiteTXT, drivingRangeTXT, rentalClubsTXT, rentalCartsTXT )
+
+            database.child(courseNameTXT).setValue(course).addOnSuccessListener {
+
+                courseName.text.clear()
+                courseAddress.text.clear()
+                numOfHoles.text.clear()
+                coursePar.text.clear()
+                yearOpened.text.clear()
+                proOnsite.text.clear()
+                drivingRange.text.clear()
+                rentalClubs.text.clear()
+                rentalCarts.text.clear()
+
+                Toast.makeText(this, "Successfully Stored", Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener {
+
+                Toast.makeText(this, "Failed to Store", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
