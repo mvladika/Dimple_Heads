285bfdbcfc90d5be1606b1ae625523fdd6a8b73e
Matthew Vladika <MJVladika@student.fullsail.edu>
2022-09-11 18:47:04 -0400

    Created custom dialog box for adding players to scorecard

---
+++ app/src/main/java/com/dimplehead/app/AddPlayerDialog.kt
@@ -0,0 +1,11 @@
+package com.dimplehead.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlayerDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_player_dialog)
+    }
+}
